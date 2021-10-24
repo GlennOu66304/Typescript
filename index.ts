cc2048dbffc9bda67702b6ef7438f4cba0bcbc43
Glenn Ou <2738328919@qq.com>
2021-10-24 16:59:16 +0800

    test

--- index.ts
+++ index.ts
@@ -1,18 +1,4 @@
-// import { Command } from "commander";
-// const program = new Command();
-
-// program
-//   .version("0.1.0")
-//   .option("-p, --peppers", "Add peppers")
-//   .option("-P, --pineapple", "Add pineapple")
-//   .option("-b, --bbq-sauce", "Add bbq sauce")
-//   .option(
-//     "-c, --cheese [type]",
-//     "Add the specified typeof cheese [marble]",
-//     "marble"
-//   )
-//   // [ts] Cannot find name 'process'.
-//   .parse(process.argv);
+import axios, { AxiosResponse } from "axios";
 import colors from "colors";
 import { Command } from "commander";
 const command = new Command();
@@ -21,7 +7,7 @@ command
   .option("-c, --city [name]", "Add city name")
   .parse(process.argv);
 const options = command.opts();
-// if (!options.city) {
+// if (!options.city)
 //   command.outputHelp();
 // }
 if (process.argv.slice(2).length === 0) {
@@ -29,7 +15,48 @@ if (process.argv.slice(2).length === 0) {
   process.exit();
 }
 
-console.log(options.city);
+interface IWeatherResponse {
+  status: string;
+  count: string;
+  info: string;
+  infocode: string;
+  lives: ILive[];
+}
 
-// yarn ts-node src/index.ts --city chengdu
-// true
+interface ILive {
+  province: string;
+  city: string;
+  adcode: string;
+  weather: string;
+  temperature: string;
+  winddirection: string;
+  windpower: string;
+  humidity: string;
+  reporttime: string;
+}
+
+const URL = "https://restapi.amap.com/v3/weather/weatherInfo";
+const KEY = "8f0a0f4ab810efdc745636f75395dd9c";
+
+axios
+  .get(
+    `${URL}?city=${encodeURI(options.city)}&key
+=${KEY}`
+  )
+  .then((res) => {
+    // (property) AxiosResponse any>.data: any
+    console.log(res.data);
+  });
+// const log = console.log;
+// axios
+//   .get(
+//     `${URL}?city=${encodeURI(options.city)}&key
+// =${KEY}`
+//   )
+//   .then((res: AxiosResponse<IWeatherResponse>) => {
+//     console.log(res.data.lives);
+//     // const live = res.data.lives[0];
+//     // log(colors.yellow(live.reporttime));
+//     // log(colors.white(`${live.province} ${live.city}`));
+//     // log(colors.green(`${live.weather} ${live.temperature}`));
+//   });
